a0028b4d5df436983adb4bbd365869e71e62161d
adil <seeopenspace@gmail.com>
2026-03-07 21:29:29 +0700

    Change example API key in config-example.js
    Updated example API key in config-example.js.

--- config-example.js
+++ config-example.js
@@ -5,7 +5,7 @@
 // To use the following configuration, create a .env file in the root of your project.
 // Add your actual API key to that .env file as shown below:
 // 
-// API_KEY=your_api_key_here
+API_KEY=AIzaSyDqNbKf_yMQ5PZdR5sqMlUgxtpnTLyDqW4
 
 // Import the dotenv package to read environment variables from .env file
 require('dotenv').config();
